cb5af02f1d76b06dd2d8710fa3ad679d2a321601
DE-corp <crazyHOPHOP@rambler.ru>
2023-04-08 15:28:08 +0300

    Задание 15.2.8

--- Module.15.LINQ2/Program.cs
+++ Module.15.LINQ2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Module._15.LINQ2
@@ -7,14 +8,29 @@ namespace Module._15.LINQ2
     {
         static void Main(string[] args)
         {
-            var numbers = new int[] { 1, 5, 3, 2, 6, 412, 43 };
-            Console.WriteLine(Average(numbers));
+            var numbers = new List<int>();
+            while (true)
+            {
+                Console.Write("Введи число: ");
+                var isNumber = int.TryParse(Console.ReadLine(), out int number);
 
-        }
+                if (!isNumber)
+                {
+                    Console.WriteLine("Нужно ввести число!");
+                    continue;
+                }
 
-        private static double Average(int[] numbers)
-        {
-            return numbers.Sum() / (double)numbers.Count();
+                numbers.Add(number);
+
+                Console.WriteLine();
+
+                Console.WriteLine($"Количество чисел в списке: " + numbers.Count());
+                Console.WriteLine($"Сумма чисел в списке: " + numbers.Sum());
+                Console.WriteLine($"Наибольшее число: " + numbers.Max());
+                Console.WriteLine($"Наименьшее число: " + numbers.Min());
+                Console.WriteLine($"Cреднее значение: " + numbers.Average());
+
+            }
         }
     }
 }
