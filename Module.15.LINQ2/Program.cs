523df0a99264b26ec7e9541caaceb424af2c9747
DE-corp <crazyHOPHOP@rambler.ru>
2023-04-08 12:18:39 +0300

    Добавьте файлы проекта.

---
+++ Module.15.LINQ2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module._15.LINQ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
