a071dc2bae03a779691ddadbae162269084b2b47
DE-corp <crazyHOPHOP@rambler.ru>
2023-04-08 12:27:12 +0300

    Задание 15.1.5

--- Module.15.LINQ2/Program.cs
+++ Module.15.LINQ2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace Module._15.LINQ2
@@ -7,13 +7,19 @@ namespace Module._15.LINQ2
     {
         static void Main(string[] args)
         {
-            var firstWord = "Привет";
-            var secondWord = "мир";
-            var list = firstWord.Intersect(secondWord);
+            var softwareManufacturers = new List<string>()
+            {
+               "Microsoft", "Apple", "Oracle"
+            };
 
-            foreach (var c in list)
-                Console.WriteLine(c);
+            var hardwareManufacturers = new List<string>()
+            {
+               "Apple", "Samsung", "Intel"
+            };
 
+            var itCompanies = softwareManufacturers.Union(hardwareManufacturers);
+            foreach (var c in itCompanies)
+                System.Console.WriteLine(c);
 
         }
     }
