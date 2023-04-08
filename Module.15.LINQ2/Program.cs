13cd799518fbf6902c7b31ea01728a7f5f6c00bb
DE-corp <crazyHOPHOP@rambler.ru>
2023-04-08 15:11:02 +0300

    Задание 15.2.3

--- Module.15.LINQ2/Program.cs
+++ Module.15.LINQ2/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Module._15.LINQ2
@@ -8,26 +7,14 @@ namespace Module._15.LINQ2
     {
         static void Main(string[] args)
         {
-            var contacts = new List<Contact>()
-            {
-               new Contact() { Name = "Андрей", Phone = 79994500508 },
-               new Contact() { Name = "Сергей", Phone = 799990455 },
-               new Contact() { Name = "Иван", Phone = 79999675334 },
-               new Contact() { Name = "Игорь", Phone = 8884994 },
-               new Contact() { Name = "Анна", Phone = 665565656 },
-               new Contact() { Name = "Василий", Phone = 3434 }
-            };
+            var numbers = new int[] { 1, 5, 3, 2, 6, 412, 43 };
+            Console.WriteLine(Average(numbers));
 
-            Console.WriteLine(contacts
-                .Where(c => c.Phone.ToString().StartsWith('7'))
-                .Where(c => c.Phone.ToString().Length == 11)
-                .Count());
         }
-    }
 
-    public class Contact
-    {
-        public string Name { get; set; }
-        public long Phone { get; set; }
+        private static double Average(int[] numbers)
+        {
+            return numbers.Sum() / (double)numbers.Count();
+        }
     }
 }
