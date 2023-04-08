626d9f9b2e506805448cde852b719accd132daff
DE-corp <crazyHOPHOP@rambler.ru>
2023-04-08 12:24:51 +0300

    Задание 15.1.4

--- Module.15.LINQ2/Program.cs
+++ Module.15.LINQ2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Module._15.LINQ2
 {
@@ -6,7 +7,14 @@ namespace Module._15.LINQ2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var firstWord = "Привет";
+            var secondWord = "мир";
+            var list = firstWord.Intersect(secondWord);
+
+            foreach (var c in list)
+                Console.WriteLine(c);
+
+
         }
     }
 }
