06b8cfc6dfa361805e54694fca42c9e5121ac958
DE-corp <crazyHOPHOP@rambler.ru>
2023-04-11 20:53:37 +0300

    Практика

--- Module.15.LINQ2/Program.cs
+++ Module.15.LINQ2/Program.cs
@@ -8,30 +8,32 @@ namespace Module._15.LINQ2
     {
         static void Main(string[] args)
         {
-          
-
-            var employees = new List<Employee>()
-           {
-               new Employee() { DepartmentId = 1, Name = "Инна", Id = 1},
-               new Employee() { DepartmentId = 1, Name = "Андрей", Id = 2},
-               new Employee() { DepartmentId = 2, Name = "Виктор ", Id = 3},
-               new Employee() { DepartmentId = 3, Name = "Альберт ", Id = 4},
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
            };
-            var employeeArray = employees.ToArray();
-            // Выводим значение до добавления элемента
-            Console.WriteLine(employeeArray.Count());
-            employees.Add(new Employee() { DepartmentId = 1, Name = "Дмитрий", Id = 5 });
-            // Выводим значение после добавления элемента
-            Console.WriteLine(employeeArray.Count());
-            // Они одинаковые, значит выполняется мгновенно
+            var allStudents = GetAllStudents(classes);
 
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            // Запрос LINQ
+            var result = from c in classes
+                         from s in c.Students
+                         select s;
+
+            // Метод расширения
+            var result1 = classes.SelectMany(c => c.Students);
+
+            return result.ToArray();
         }
     }
 
-    internal class Employee
+    public class Classroom
     {
-        public int DepartmentId { get; set; }
-        public string Name { get; set; }
-        public int Id { get; set; }
+        public List<string> Students = new List<string>();
     }
 }
